e542184dc76dcc217e9bdf74d30ffba0e956dc2f
SALEORSHOP <69590337+SALEORSHOP@users.noreply.github.com>
2020-11-17 11:41:55 -0800

    Commit

---
+++ onlineshop/src/main/java/in/shop/onlineshop/OnlineShopStoreApplication.java
@@ -0,0 +1,13 @@
+package in.shop.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopStoreApplication.class, args);
+	}
+
+}
